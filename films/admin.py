a1b1d9dd855828de37de997788ff831f1c341f47
Tiger_CDev <tigerchoopaibool@gmail.com>
2026-05-08 01:23:20 +0700

    Add Film and Cinematographer models with migrations

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cinematographer, Film
 
 # Register your models here.
+admin.site.register(Cinematographer)
+admin.site.register(Film)
--- films/models.py
+++ films/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Cinematographer(models.Model):
+    name = models.CharField(max_length=255)
+    birth_year = models.IntegerField(null=True, blank=True)
+    nationality = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Film(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=255)
+    Cinematographer = models.ForeignKey(
+        Cinematographer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='films',
+    )
+    aspect_ratio = models.CharField(max_length=20, null=True, blank=True)
+    runtime = models.IntegerField(null=True, blank=True)
+    synopsis = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
