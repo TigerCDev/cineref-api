ef3032118c591bb236aafba87221e9380033e63b
Tiger_CDev <tigerchoopaibool@gmail.com>
2026-05-16 20:58:19 +0700

    add JWT authentication and protect write endpoints

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,15 @@ INSTALLED_APPS = [
     'films',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
