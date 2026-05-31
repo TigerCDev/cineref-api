239300581d75dd1e6e298bfd0a675643911fbc5c
Tiger_CDev <tigerchoopaibool@gmail.com>
2026-05-31 23:50:13 +0700

    Deploy to Fly.io - CineRef API live

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,12 +1,21 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split()
 
 # Security headers
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SECURE_HSTS_SECOND = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
